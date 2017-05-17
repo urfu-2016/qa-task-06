5afdad9691bef86ee66280da5d1f39d0bb3061cb
Tsvetkova Valeria <lera.tsvetkova96@gmail.com>
2017-05-17 18:32:32 +0500

    Тесты

--- tests/mongomart-test.js
+++ tests/mongomart-test.js
@@ -1,5 +1,50 @@
-describe('Mongomart', () => {
-    it('should ...', () => {
-        browser.url('http://urfu-2016-testing.herokuapp.com/');
+const assert = require('assert');
+const moment = require('moment');
+const URL = 'http://urfu-2016-testing.herokuapp.com/';
+
+describe('Search of products with not an empty result', () => {
+
+    before(() => {
+        browser
+            .url(URL)
+            .setValue('input[name="query"]', 'pen')
+            .click('button[type="submit"]');
+    });
+
+    it('should show right amount of found products', () => {
+        var productAmount = browser.getText('i');
+        assert.equal(productAmount, '2 Products');
+    });
+
+    it('should show right bread crumbs', () => {
+        var crumbs = browser.getText('.breadcrumb > li');
+        assert.equal(crumbs[0], 'Home');
+        assert.equal(crumbs[1], 'Search');
+        assert.equal(crumbs[2], '"pen"');
+    });
+});
+
+describe('Correct bread crumbs', () => {
+
+    it('should show right bread crumbs for main page', () => {
+        browser.url(URL);
+        var crumbs = browser.getText('.breadcrumb > li');
+        assert.equal(crumbs[0], 'Home');
+        assert.equal(crumbs[1], 'All');
+    });
+
+    it('should show right bread crumbs for chosen category', () => {
+        browser.url('http://urfu-2016-testing.herokuapp.com/?category=Electronics');
+        var crumbs = browser.getText('.breadcrumb > li');
+        assert.equal(crumbs[0], 'Home');
+        assert.equal(crumbs[1], 'Electronics');
+    });
+
+    it('should show right bread crumbs for chosen product', () => {
+        browser.url('http://urfu-2016-testing.herokuapp.com/item/3');
+        var crumbs = browser.getText('.breadcrumb > li');
+        assert.equal(crumbs[0], 'Home');
+        assert.equal(crumbs[1], 'Swag');
+        assert.equal(crumbs[2], 'Stress Ball');
     });
 });
