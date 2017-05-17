3081c2efe787166c46c1f593a8eefffa4a5a3e82
Кулаев Игорь <kulaevmath@gmail.com>
2017-05-17 16:57:27 +0300

    Update mongomart-test.js

--- tests/mongomart-test.js
+++ tests/mongomart-test.js
@@ -1,5 +1,31 @@
-describe('Mongomart', () => {
-    it('should ...', () => {
-        browser.url('http://urfu-2016-testing.herokuapp.com/');
+const assert = require('assert');
+
+const URL = 'http://urfu-2016-testing.herokuapp.com/';
+
+
+
+describe('Search of products with not an empty result', () => {
+
+
+
+    before(() => {
+
+        browser
+
+            .url(URL)
+
+            .setValue('input[name="query"]', 'gray')
+
+            .click('button[type="submit"]');
+
     });
-});
+
+
+
+    it('should show right amount of found products', () => {
+
+        var productAmount = browser.getText('i');
+
+        assert.equal(productAmount, '2 Products');
+
+    });
