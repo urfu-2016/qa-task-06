228c1af7df5029cc518dade702e187eb5e7be335
Лилия Шаламова < >
2017-05-22 20:09:20 +0500

    Замечания

--- tests/mongomart-test.js
+++ tests/mongomart-test.js
@@ -1,15 +1,26 @@
 const assert = require('assert');
 const moment = require('moment');
 
-describe('Seaching with the found result', () => {
+describe('Searching with the found result', () => {
 	
 	before (() => {
 		browser
 			.url('/')
-			.setValue('input[placeholder="Search"]', 'Pen')
-			.click('button[type="submit"]');
+			.setValue('input[class="form-control"]', 'Pen')
+			.click('button[class="btn btn-default"]');
 	});
 	
+	it('should check that the description or the title has words from a search', () => {
+		var request = "pen";
+		const products_descriptions = browser.getText('.col-md-5');
+		products_descriptions.forEach(description => {
+			var description = description.toLowerCase();
+			var tmp = description.split('\n').map(row => row.split(' '));
+			var words = tmp[0].concat(tmp[1]);
+			assert(words.indexOf(request) !== -1);
+		});
+	});
+
 	it('should return 2 rows', () => {
 		const rows = browser.elements('.row h4');
 		assert.equal(2, rows.value.length);
@@ -27,9 +38,16 @@ describe('Seaching with the found result', () => {
 	it('should show right count of products', () => {
 		const products = browser.getText('i');
 		assert.equal(products, "2 Products");
-	});
+	});	
+});
 
-	it('should show right bread crumbs', () => {
+
+describe('Bread crumbs', () => {
+	it('should show right bread crumbs when looking for a product', () => {
+		browser
+			.url('/')
+			.setValue('input[class="form-control"]', 'Pen')
+			.click('button[class="btn btn-default"]');
 		const productName = browser.getText('li[class="active"] ');
 		const categoryName = browser.getText('ol[class="breadcrumb"] li:nth-child(2)');
 		const home = browser.getText('li a');
@@ -38,16 +56,8 @@ describe('Seaching with the found result', () => {
 		assert.equal(home, 'Home');
 	});
 	
-});
-
-
-describe('Bread crumbs', () => {
-	
-	it('should return 2 rows', () => {
-	    browser
-			.url('/')
-			.click('a[href="/?category=Books"]')
-			.click('a[href="/item/19"]');
+	it('should show right bread crumbs for navigate to the product card', () => {
+	    browser.url("/item/19");
 		const productName = browser.getText('li[class="active"] ');
 		const categoryName = browser.getText('ol[class="breadcrumb"] li:nth-child(2)');
 		const home = browser.getText('li:first-child a');
@@ -55,22 +65,48 @@ describe('Bread crumbs', () => {
 		assert.equal(categoryName, 'Books');
 		assert.equal(home, 'Home');
     });
+	
+	it('should show right bread crumbs in cart', () => {
+	    browser
+			.url('/')
+			.click('button[class="btn btn-success"]');
+		const home = browser.getText('ol[class="breadcrumb"] li:first-child a');
+		const cart = browser.getText('li[class="active"] ');
+		assert.equal(cart, "Cart");
+		assert.equal(home, 'Home');
+    });
+	
+	it('should show right bread crumbs in category', () => {
+	    browser.url('/?category=Books');
+		const home = browser.getText('ol[class="breadcrumb"] li:first-child a');
+		const categoryName = browser.getText('li[class="active"] ');
+		assert.equal(categoryName, "Books");
+		assert.equal(home, 'Home');
+    });
+	
+	it('should show right bread crumbs on the main page', () => {
+		browser.url('/');
+		const home = browser.getText('ol[class="breadcrumb"] li:first-child a');
+		const all = browser.getText('ol li[class="active"] ');
+		assert.equal(all, "All");
+		assert.equal(home, 'Home');
+    });
 });
 
 describe('Date and time of product recall', () => {
+	var reviewDate;
 	before(() => {
 		browser
-			.url('/')
-			.click('a[href="/?category=Books"]')
-			.click('a[href="/item/19"]')
+			.url('/item/19')
 			.setValue('textarea[name="review"]', "Review")
 			.setValue('input[name="name"]', 'name')
 			.click('input[value="1"]')
 			.click('form:nth-child(2) button[type="submit"]');
+			reviewDate = new Date();
 	});
 	
 	it('should return right name', () => {
-		const comments = browser.getText('.container .row div:last-child h4');
+		const comments = browser.getText('.container .row div:last-child .media-heading');
 		const name = comments[comments.length - 1].split(" ")[0];
 		assert.equal(name, 'name');
 	});
@@ -83,9 +119,17 @@ describe('Date and time of product recall', () => {
 	
 	it('should return right date format', () => {
 	
-		const comments = browser.getText('.container .row div:last-child h4');
+		const comments = browser.getText('.container .row div:last-child .media-heading');
 		const date = comments[comments.length - 1].split(" ").slice(1).join(" ");
 		assert(moment(date, 'MMMM Do YYYY, h:m:s a').isValid());
 		
 	});
+	
+	it('should return right time', () => {
+        const comments = browser.getText('.container .row div:last-child .media-heading');
+        var date = comments[comments.length - 1].split(" ").slice(1).join(" ");
+        date = new Date(moment.utc(date, 'MMMM Do YYYY, h:m:s a'));
+		assert(Math.abs(date - reviewDate) < 30 * 1000);
+	});
 });
+
